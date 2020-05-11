9fdad02ce1216b0481ac66b382d128f0446e70a1
sethroblucas <sethroblucas@gmail.com>
2020-05-11 16:08:48 -0400

    starting javaScript basics
    starting javaScript basics

---
+++ 2-JavaScript Basics/script.js
@@ -0,0 +1,55 @@
+//**************************** */
+// VARIABLES AND DATA TYPES
+/* var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job); */
+
+//**************************** */
+// VARIABLE MUTATION AND TYPE CORRECTION
+
+/* var firstName = "John";
+var age = 28;
+
+console.log(firstName + firstName + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+// VARIABLE MUTATION
+age = "twenty eight";
+job = "driver";
+
+alert(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+var lastName = prompt("What is his last Name?");
+console.log(firstName + " " + lastName); */
