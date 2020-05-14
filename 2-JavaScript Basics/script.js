df2deda6c6e13f85a1e1099130c9f77645cb5116
sethroblucas <sethroblucas@gmail.com>
2020-05-14 19:00:22 -0400

    worked on JavaScript course
    worked on JavaScript course

--- 2-JavaScript Basics/script.js
+++ 2-JavaScript Basics/script.js
@@ -327,3 +327,145 @@ if (maryAverage > johnAverage && maryAverage > mikeAverage) {
 
 //************************************************* */
 // FUNCTIONS
+
+/* function calculateAge(birthYear) {
+  return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(firstName + " retires in " + retirement + " years.");
+  } else {
+    console.log(firstName + " is already retired.");
+  }
+}
+
+yearsUntilRetirement(1990, "John");
+yearsUntilRetirement(1948, "Mike");
+yearsUntilRetirement(1969, "Jane"); */
+
+//************************************************* */
+// FUNCTION STATEMENTS AND EXPRESSIONS
+
+// FUNCTION EXPRESSION
+/* var whatDoYouDo = function (job, firstName) {
+  switch (job) {
+    case "teacher":
+      return firstName + " teaches kids how to code";
+    case "driver":
+      return firstName + " drives a cab in Lisbon.";
+    case "designer":
+      return firstName + " designs beautiful websites";
+    default:
+      return firstName + " does something else";
+  }
+};
+
+console.log(whatDoYouDo("teacher", "John"));
+console.log(whatDoYouDo("designer", "Jane"));
+console.log(whatDoYouDo("retired", "Mark")); */
+
+//************************************************* */
+// ARRAYS
+
+// INITIALIZE ARRAY
+/* var names = ["John", "Mark", "Jane"];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+console.log(names);
+console.log(names.length);
+
+// MUTATE ARRAY
+names[1] = "Ben";
+console.log(names);
+names[names.length] = "Mary";
+console.log(names);
+
+// DIFFERENT
+var john = ["John", "Smith", 1990, "teacher", false];
+
+john.push("blue");
+john.unshift("Mr.");
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner =
+  john.indexOf("designer") === -1
+    ? "John is NOT a designer"
+    : "John IS a designer";
+console.log(isDesigner); */
+
+//************************************************* */
+// CODING CHALLENGE #3
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+GOOD LUCK 😀
+*/
+
+// STEP #1 & 2
+/* var beforeBill = [124, 48, 268];
+var tips = [];
+var afterBill = [];
+
+var totalCalc = function (bill) {
+  if (bill < 50) {
+    return bill + bill * 0.2;
+  } else if (bill >= 50 && bill <= 200) {
+    return bill + bill * 0.15;
+  } else {
+    return bill + bill * 0.1;
+  }
+};
+
+var tipCalc = function (bill) {
+  if (bill < 50) {
+    return bill * 0.2;
+  } else if (bill >= 50 && bill <= 200) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.1;
+  }
+};
+
+var tipOne = tipCalc(beforeBill[0]);
+var tipTwo = tipCalc(beforeBill[1]);
+var tipThree = tipCalc(beforeBill[2]);
+
+tips[tips.length] = tipOne;
+tips[tips.length] = tipTwo;
+tips[tips.length] = tipThree;
+console.log(tips);
+
+var billOne = totalCalc(beforeBill[0]);
+var billTwo = totalCalc(beforeBill[1]);
+var billThree = totalCalc(beforeBill[2]);
+
+afterBill[afterBill.length] = billOne;
+afterBill[afterBill.length] = billTwo;
+afterBill[afterBill.length] = billThree;
+console.log(afterBill); */
+
+//************************************************* */
