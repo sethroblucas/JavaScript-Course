2b11d66dddcb5a4676c17a4930ddf40d2e8ea73b
sethroblucas <sethroblucas@gmail.com>
2020-05-18 16:54:12 -0400

    Worked on JavaScript course 4
    Worked on JavaScript course 4

--- 4-JavaScript DOM/starter/app.js
+++ 4-JavaScript DOM/starter/app.js
@@ -38,22 +38,46 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
   } else {
     // next player
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-
-    //document.querySelector(".player-0-panel").classList.remove("active");
-    //document.querySelector(".player-1-panel").classList.add("active");
-
-    document.querySelector(".dice").style.display = "none";
+    nextPlayer();
   }
 });
 
-// document.querySelector("#current-' + activePlayer").textContent = dice;
-// // document.querySelector("#current-" + activePlayer).innerHTML =
-// //   "<em>" + dice + "</em>";
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  // ADD CURRENT SCORE TO GLOBAL SCORE
+  scores[activePlayer] += roundScore;
+  // UPDATE THE UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  // CHECK IF PLAYER WON THE GAME
+  if (scores[activePlayer] >= 100) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    // next player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  // next player
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  //document.querySelector(".player-0-panel").classList.remove("active");
+  //document.querySelector(".player-1-panel").classList.add("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
