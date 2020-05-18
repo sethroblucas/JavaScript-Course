71329fb55be73f5c41922997287de0ac69295bf7
sethroblucas <sethroblucas@gmail.com>
2020-05-17 23:18:47 -0400

    worked on JavaScript course 4
    worked on JavaScript course 4

--- 4-JavaScript DOM/starter/app.js
+++ 4-JavaScript DOM/starter/app.js
@@ -15,10 +15,45 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector("#current-' + activePlayer").textContent = dice;
-// document.querySelector("#current-" + activePlayer).innerHTML =
-//   "<em>" + dice + "</em>";
-
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1. random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. display result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3, update the round score if the rolled number was not a 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //document.querySelector(".player-0-panel").classList.remove("active");
+    //document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
+});
+
+// document.querySelector("#current-' + activePlayer").textContent = dice;
+// // document.querySelector("#current-" + activePlayer).innerHTML =
+// //   "<em>" + dice + "</em>";
