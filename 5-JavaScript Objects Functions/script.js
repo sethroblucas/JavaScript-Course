844abd19a3e90896411ac015c4c1809241108ff6
sethroblucas <sethroblucas@gmail.com>
2020-05-20 22:35:53 -0400

    worked on JavaScript sec 5
    worked on JavaScript sec 5

--- 5-JavaScript Objects Functions/script.js
+++ 5-JavaScript Objects Functions/script.js
@@ -33,7 +33,7 @@ console.log(mark.lastName); */
 
 // ***********************************************
 // OBJECT.CREATE
-var personProto = {
+/* var personProto = {
   calculateAge: function () {
     console.log(2016 - this.yearOfBirth);
   },
@@ -48,4 +48,106 @@ var jane = Object.create(personProto, {
   name: { value: "Jane" },
   yearOfBirth: { value: 1969 },
   job: { value: "designer" },
-});
+}); */
+
+// ***********************************************
+// PRIMITIVES vs. OBJECTS
+
+// primitives
+/* var a = 23;
+var b = a;
+a = 46;
+console.log(a, b);
+
+// objects
+var obj1 = {
+  name: "John",
+  age: 26,
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age, obj2.age);
+
+// functions
+var age = 27;
+var obj = {
+  name: "Jonas",
+  city: "Lisbon",
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = "San Fran";
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city); */
+
+// ***********************************************
+// PASSING FUNCTIONS AS ARGUMENTS
+
+/* var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates); */
+
+// ***********************************************
+// FUNCTIONS RETURNING FUNCTIONS
+
+/* function interviewQuestion(job) {
+  if (job === "designer") {
+    return function (name) {
+      console.log(name + ", can you please explain UX?");
+    };
+  } else if (job === "teacher") {
+    return function (name) {
+      console.log(name + ", what do you teach?");
+    };
+  } else {
+    return function (name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+interviewQuestion("teacher")("Mark"); */
+
+// ***********************************************
+// LECTURE: IIFE
